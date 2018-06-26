c2efd2727e754a89e54eded27c1278e79389f4ac
uidevelopmentwithangular <bandimahendranath.reddy@gmail.com>
2018-06-27 00:42:17 +0530

    Gulp With Angular JS Scratch

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+
+
+ angular.module('helloWorldApp', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider){
+           $routeProvider.when('/', {
+               templateUrl : 'views/home.html',
+               controller : 'HomeCtrl'
+           })
+        }
+    ]);
---
+++ src/js/controllers/HomeCtrl.js
@@ -0,0 +1,6 @@
+
+    angular.module('helloWorldApp')
+    .controller('HomeCtrl', ['$scope', function($scope){
+        $scope.message ="Hello world !";
+    }
+]);
